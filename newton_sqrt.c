16f0bd1be2f21a5cc6fcd0874219dd75c29e9662
gaoyuY <2282940822@qq.com>
2018-09-14 11:57:30 +0800

    update

--- newton_sqrt.c
+++ newton_sqrt.c
@@ -4,16 +4,18 @@
 	> Mail:2282940822 
 	> Created Time: 2018年09月13日 星期四 21时24分46秒
  ************************************************************************/
+/***************牛顿迭代最终公式: x1 = x - f(x) / f'(x); *********************************************/
+
 
 #include<stdio.h>
 #include<math.h>
 #define EPS 1e-7
 
-double f1 (double x, double n) {
+double f1 (double x, double n) {   //f(x) = x^2 - n;
     return x * x - n;
 }
 
-double f1_prime(double x) {
+double f1_prime(double x) {  //f(x) = x^2 - n;  f'(x) = 2 * x; 
     return 2 * x;
 }
 
@@ -26,7 +28,7 @@ double newton_sqrt(double n, double (*f)(double, double), double(*f_prime)(doubl
 }
 int main () {
     double x; 
-    while (scanf ("%lf", &x) != EOF) {
+    while (scanf ("%lf", &x) != EOF) {  //求x的平方根
         printf ("%lf\n", newton_sqrt(x, f1, f1_prime));
     }
 
